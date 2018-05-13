e20a95ec4a7d7f698ced58d3fbe59a4ceab99d0f
Jen <jenniferanndcb@gmail.com>
2018-05-13 20:47:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function yourAnimal() {
 function add2(n) {
   const two = 2 
   return n + two
-
+}
   // Feel free to move things around!
   
-}
+
 
 var funkyFunction = function() {
   return function() {
